f1761f4987e66e5479450b6e9e8e5f54a7daf7be
iot.mac.mini <iot_mac_mini@iotmacminis-Mac-mini.local>
2024-05-07 17:29:22 +0800

    ss

--- algorithm2.py
+++ algorithm2.py
@@ -6,49 +6,57 @@ from tomlkit import string
 import matplotlib.pyplot as plt
 import math
 c=0.1
-REPEAT_COUNT=10
+REPEAT_COUNT=1000
 
-TARGET_COUNT=0
-PERCENT1_COUNT=0
+
+
 RESULT_DIC=[]
 X=[]
-for i in range(REPEAT_COUNT):
-    X.append(i)
-for k in range(REPEAT_COUNT):
-    X1=[]
-    res = []
-    itemss = 10
-    for i in range(itemss):
-        random_number = random.random()
-        X1.append(random_number)
-        res.append(X1[i]-i*c)
-    res.sort()
-    Vtemp=0
-    if (c<=0.5) :
-        Vtemp=1-math.sqrt(2*c)
-    else :
-        Vtemp=-c+1/2
-    Maxpos=itemss-1
-    Maxval=X1[itemss-1]-c*itemss
-    for i in range(itemss):
-        if X1[i] >= Vtemp:
-            Maxpos=i
-            Maxval=X1[i]-i*c
-            break
-    Range=0
-    for j in range(itemss):
-        if(Maxval==res[j]):
-            Range=j
-    RESULT_DIC.append(Range+1)
-    if Range>=itemss-1:
-        TARGET_COUNT+=1
-    if Range>=itemss*0.99:
-        PERCENT1_COUNT += 1
+Rate1=[]
+Rate1pct=[]
+Rate10pct=[]
+for itemss in range(1000,10000,10):
+    PERCENT1_COUNT=0
+    PERCENT10_COUNT=0
+    TARGET_COUNT=0
+    X.append(itemss)
+    for k in range(REPEAT_COUNT):
+        X1=[]
+        res = []
+        for i in range(itemss):
+            random_number = random.random()
+            X1.append(random_number)
+            res.append(X1[i]-i*c)
+        res.sort()
+        Vtemp=0
+        if (c<=0.5) :
+            Vtemp=1-math.sqrt(2*c)
+        else :
+            Vtemp=-c+1/2
+        Maxpos=itemss-1
+        Maxval=X1[itemss-1]-c*itemss
+        for i in range(itemss):
+            if X1[i] >= Vtemp:
+                Maxpos=i
+                Maxval=X1[i]-i*c
+                break
+        Range=0
+        for j in range(itemss):
+            if(Maxval==res[j]):
+                Range=j
+        RESULT_DIC.append(Range+1)
+        if Range>=itemss-1:
+            TARGET_COUNT += 1
+        if Range>=itemss*0.99:
+            PERCENT1_COUNT += 1
+        if Range>=itemss*0.9:
+            PERCENT10_COUNT += 1
+    Rate1.append(TARGET_COUNT/REPEAT_COUNT)
+    Rate1pct.append(PERCENT1_COUNT/REPEAT_COUNT)
+    Rate10pct.append(PERCENT10_COUNT/REPEAT_COUNT)
 
 
-print("最大值命中率:")
-print(float(TARGET_COUNT/REPEAT_COUNT))
-print("前1%命中率:")
-print(float(PERCENT1_COUNT/REPEAT_COUNT))
-plt.scatter(X,RESULT_DIC)
+plt.scatter(X,Rate1,color="blue")
+plt.scatter(X,Rate1pct,color="green")
+plt.scatter(X,Rate10pct,color="red")
 plt.show()
---
+++ code33.py
@@ -0,0 +1,8 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+plt.scatter([1,2,3,4,5],[4,5,6,7,3],color="blue")
+plt.scatter([1,2,3,4,5],[6,10,9,3,14],color="green")
+plt.scatter([1,2,3,4,5],[5,17,2,1,19],color="red")
+plt.show()
