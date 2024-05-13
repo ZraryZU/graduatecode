862b07059cde28831d834e2b78f3db975eab97cb
iot.mac.mini <iot_mac_mini@iotmacminis-Mac-mini.local>
2024-05-13 17:29:42 +0800

    sss

--- code33.py
+++ code33.py
@@ -1,8 +1,43 @@
-import numpy as np
-import random
-import matplotlib.pyplot as plt
+import torch
+from torch.autograd import Variable
 
-plt.scatter([1,2,3,4,5],[4,5,6,7,3],color="blue")
-plt.scatter([1,2,3,4,5],[6,10,9,3,14],color="green")
-plt.scatter([1,2,3,4,5],[5,17,2,1,19],color="red")
-plt.show()
+batch_n = 64  # 一个批次输入数据的数量
+hidden_layer = 100
+input_data = 1000  # 每个数据的特征为1000
+output_data = 10
+
+
+class Model(torch.nn.Module):  # 完成类继承的操作
+    def __init__(self):
+        super(Model, self).__init__()  # 类的初始化
+
+    def forward(self, input, w1, w2):
+        x = torch.mm(input, w1)
+        x = torch.clamp(x, min=0)
+        x = torch.mm(x, w2)
+        return x
+
+    def backward(self):
+        pass
+
+
+model = Model()
+x = Variable(torch.randn(batch_n, input_data), requires_grad=False)
+y = Variable(torch.randn(batch_n, output_data), requires_grad=False)
+# 用Variable对Tensor数据类型变量进行封装的操作。requires_grad如果是F，表示该变量在进行自动梯度计算的过程中不会保留梯度值。
+w1 = Variable(torch.randn(input_data, hidden_layer), requires_grad=True)
+w2 = Variable(torch.randn(hidden_layer, output_data), requires_grad=True)
+
+epoch_n = 30
+
+for epoch in range(epoch_n):
+    y_pred = model(x, w1, w2)
+
+    loss = (y_pred - y).pow(2).sum()
+    print("epoch:{},loss:{:.4f}".format(epoch, loss.data))
+    loss.backward()
+    w1.data -= lr * w1.grad.data
+    w2.data -= lr * w2.grad.data
+
+    w1.grad.data.zero_()
+    w2.grad.data.zero_()
--- test1(1).py
+++ test1(1).py
@@ -1,19 +1,68 @@
+import torch.nn as nn
+import torch.functional as F
 import numpy as np
-import random
-import matplotlib
+import torch
+from torch.utils.data import Dataset
 
-def generate_bernulli(p):
-    random_nums=random.random()
-    if random_nums<=p:
-        return 1
-    else:
-        return 0
-sum=0
-nums=10000
-for _ in range(nums):
-    #print(generate_bernulli(0.5)) 
-    random_bonulli= np.random.binomial(1,0.5)
-    print(random_bonulli)
-    
-    sum+=random_bonulli
-print(sum/nums)
+
+class Model(nn.Module):
+    def __init__(self):
+        super.__init__()
+        self.conv1 = nn.Conv2d(1,20,5)
+        self.conv2 = nn.Conv2d(20,20,5)
+
+    def forward(self,x):
+        x = F.relu(self.conv1(x))
+        return F.relu(self.conv2(x))
+
+class ImageDataset(Dataset):
+    def __init__(self, raw_data)
+        self.raw_data = raw_data
+
+    def __len__(self)
+        return len(self.raw_data)
+
+    def __getitem__(self, index)
+        image, lable = self.raw_data[index]
+        return image, lable
+
+# 标量Tensor求导
+# 求 f(x) = a*x**2 + b*x + c 的导数
+x = torch.tensor(-2.0, requires_grad=True)
+a = torch.tensor(1.0)
+b = torch.tensor(2.0)
+c = torch.tensor(3.0)
+y = a*torch.pow(x,2)+b*x+c
+y.backward() # backward求得的梯度会存储在自变量x的grad属性中
+dy_dx =x.grad
+print(dy_dx)
+
+
+
+
+
+
+# f(x) = a*x**2 + b*x + c的最小值
+x = torch.tensor(8.0, requires_grad=True)  # x需要被求导
+z = torch.tensor(0.0, requires_grad=True)  #
+
+x = torch.tensor([[-2.0,-1.0],[0.0,1.0]], requires_grad=True)
+a = torch.tensor(1.0)
+b = torch.tensor(-2.0)
+c = torch.tensor(1.0)
+gradient=torch.tensor([[1.0,1.0],[1.0,1.0]])
+optimizer = torch.optim.SGD(params=[x], lr=0.01)  # SGD为随机梯度下降
+print(optimizer)
+
+
+def f(x):
+    result = a * torch.pow(x, 2) + b * x + c
+    return (result)
+
+
+for i in range(500):
+    optimizer.zero_grad()  # 将模型的参数初始化为0
+    y = f(x)
+    y.backward(gradient=gradient)  # 反向传播计算梯度
+    optimizer.step()  # 更新所有的参数
+print("y=", y.data, ";", "x=", x.data)
